1606c9e4a454f12a6d41184e23f9a8c9c93c50ae
Ahmad2255 <ahmadian225@gmail.com>
2021-06-09 13:43:36 +0300

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('5 kilos are ready !'));
+app.get('/', (req, res) => res.send('544 kilos are ready !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
